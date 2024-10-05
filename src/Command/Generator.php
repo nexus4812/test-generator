e4d90fac203d43551884bfc87d6720317005569f
nexus4812 <powerbatteryboy@gmail.com>
2024-10-05 18:29:07 +0900

    delete unused packages

--- src/Command/Generator.php
+++ src/Command/Generator.php
@@ -8,7 +8,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 // the name of the command is what users type after "php bin/console"
-#[AsCommand(name: 'create-unit-test')]
 class Generator extends Command
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
